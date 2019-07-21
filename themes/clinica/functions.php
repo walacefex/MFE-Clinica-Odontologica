447b0ea04e57cf1bbc5866711fdfb820f252ad00
jakeliny.gr@gmail.com <jakeliny.gr@gmail.com>
2019-07-21 17:46:08 -0300

    puxando conteudo dos post types no front + adicionando depoimentos

--- themes/clinica/functions.php
+++ themes/clinica/functions.php
@@ -16,8 +16,8 @@
 /**
  * Sets content width.
  */
-if ( ! isset( $content_width ) ) {
-	$content_width = 600;
+if (!isset($content_width)) {
+    $content_width = 600;
 }
 
 /**
@@ -44,216 +44,220 @@ require_once get_template_directory() . '/core/classes/class-metabox.php';
  */
 require_once get_template_directory() . '/core/classes/widgets/class-widget-like-box.php';
 
-if ( ! function_exists( 'odin_setup_features' ) ) {
+if (!function_exists('odin_setup_features')) {
 
-	/**
-	 * Setup theme features.
-	 *
-	 * @since 2.2.0
-	 */
-	function odin_setup_features() {
+    /**
+     * Setup theme features.
+     *
+     * @since 2.2.0
+     */
+    function odin_setup_features()
+    {
 
-		/**
-		 * Add support for multiple languages.
-		 */
-		load_theme_textdomain( 'odin', get_template_directory() . '/languages' );
+        /**
+         * Add support for multiple languages.
+         */
+        load_theme_textdomain('odin', get_template_directory() . '/languages');
 
-		/**
-		 * Register nav menus.
-		 */
-		register_nav_menus(
-			array(
-				'main-menu' => __( 'Main Menu', 'odin' )
-			)
-		);
+        /**
+         * Register nav menus.
+         */
+        register_nav_menus(
+            array(
+                'main-menu' => __('Main Menu', 'odin'),
+            )
+        );
 
-		/*
-		 * Add post_thumbnails suport.
-		 */
-		add_theme_support( 'post-thumbnails' );
+        /*
+         * Add post_thumbnails suport.
+         */
+        add_theme_support('post-thumbnails');
 
-		/**
-		 * Add feed link.
-		 */
-		add_theme_support( 'automatic-feed-links' );
+        /**
+         * Add feed link.
+         */
+        add_theme_support('automatic-feed-links');
 
-		/**
-		 * Support Custom Header.
-		 */
-		$default = array(
-			'width'         => 0,
-			'height'        => 0,
-			'flex-height'   => false,
-			'flex-width'    => false,
-			'header-text'   => false,
-			'default-image' => '',
-			'uploads'       => true,
-		);
+        /**
+         * Support Custom Header.
+         */
+        $default = array(
+            'width' => 0,
+            'height' => 0,
+            'flex-height' => false,
+            'flex-width' => false,
+            'header-text' => false,
+            'default-image' => '',
+            'uploads' => true,
+        );
 
-		add_theme_support( 'custom-header', $default );
+        add_theme_support('custom-header', $default);
 
-		/**
-		 * Support Custom Background.
-		 */
-		$defaults = array(
-			'default-color' => '',
-			'default-image' => '',
-		);
+        /**
+         * Support Custom Background.
+         */
+        $defaults = array(
+            'default-color' => '',
+            'default-image' => '',
+        );
 
-		add_theme_support( 'custom-background', $defaults );
+        add_theme_support('custom-background', $defaults);
 
-		/**
-		 * Support Custom Editor Style.
-		 */
-		add_editor_style( 'assets/css/editor-style.css' );
+        /**
+         * Support Custom Editor Style.
+         */
+        add_editor_style('assets/css/editor-style.css');
 
-		/**
-		 * Add support for infinite scroll.
-		 */
-		add_theme_support(
-			'infinite-scroll',
-			array(
-				'type'           => 'scroll',
-				'footer_widgets' => false,
-				'container'      => 'content',
-				'wrapper'        => false,
-				'render'         => false,
-				'posts_per_page' => get_option( 'posts_per_page' )
-			)
-		);
+        /**
+         * Add support for infinite scroll.
+         */
+        add_theme_support(
+            'infinite-scroll',
+            array(
+                'type' => 'scroll',
+                'footer_widgets' => false,
+                'container' => 'content',
+                'wrapper' => false,
+                'render' => false,
+                'posts_per_page' => get_option('posts_per_page'),
+            )
+        );
 
-		/**
-		 * Add support for Post Formats.
-		 */
-		// add_theme_support( 'post-formats', array(
-		//     'aside',
-		//     'gallery',
-		//     'link',
-		//     'image',
-		//     'quote',
-		//     'status',
-		//     'video',
-		//     'audio',
-		//     'chat'
-		// ) );
+        /**
+         * Add support for Post Formats.
+         */
+        // add_theme_support( 'post-formats', array(
+        //     'aside',
+        //     'gallery',
+        //     'link',
+        //     'image',
+        //     'quote',
+        //     'status',
+        //     'video',
+        //     'audio',
+        //     'chat'
+        // ) );
 
-		/**
-		 * Support The Excerpt on pages.
-		 */
-		// add_post_type_support( 'page', 'excerpt' );
+        /**
+         * Support The Excerpt on pages.
+         */
+        // add_post_type_support( 'page', 'excerpt' );
 
-		/**
-		 * Switch default core markup for search form, comment form, and comments to output valid HTML5.
-		 */
-		add_theme_support(
-			'html5',
-			array(
-				'search-form',
-				'comment-form',
-				'comment-list',
-				'gallery',
-				'caption'
-			)
-		);
+        /**
+         * Switch default core markup for search form, comment form, and comments to output valid HTML5.
+         */
+        add_theme_support(
+            'html5',
+            array(
+                'search-form',
+                'comment-form',
+                'comment-list',
+                'gallery',
+                'caption',
+            )
+        );
 
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support( 'title-tag' );
+        /*
+         * Let WordPress manage the document title.
+         * By adding theme support, we declare that this theme does not use a
+         * hard-coded <title> tag in the document head, and expect WordPress to
+         * provide it for us.
+         */
+        add_theme_support('title-tag');
 
-		/*
-		 * Enable support for custom logo.
-		 *
-		 *  @since Odin 2.2.10
-		 */
-		add_theme_support( 'custom-logo', array(
-			'height'      => 240,
-			'width'       => 240,
-			'flex-height' => true,
-		) );
-	}
+        /*
+         * Enable support for custom logo.
+         *
+         *  @since Odin 2.2.10
+         */
+        add_theme_support('custom-logo', array(
+            'height' => 240,
+            'width' => 240,
+            'flex-height' => true,
+        ));
+    }
 }
 
-add_action( 'after_setup_theme', 'odin_setup_features' );
+add_action('after_setup_theme', 'odin_setup_features');
 
 /**
  * Register widget areas.
  *
  * @since 2.2.0
  */
-function odin_widgets_init() {
-	register_sidebar(
-		array(
-			'name' => __( 'Main Sidebar', 'odin' ),
-			'id' => 'main-sidebar',
-			'description' => __( 'Site Main Sidebar', 'odin' ),
-			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-			'after_widget' => '</aside>',
-			'before_title' => '<h3 class="widgettitle widget-title">',
-			'after_title' => '</h3>',
-		)
-	);
+function odin_widgets_init()
+{
+    register_sidebar(
+        array(
+            'name' => __('Main Sidebar', 'odin'),
+            'id' => 'main-sidebar',
+            'description' => __('Site Main Sidebar', 'odin'),
+            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+            'after_widget' => '</aside>',
+            'before_title' => '<h3 class="widgettitle widget-title">',
+            'after_title' => '</h3>',
+        )
+    );
 }
 
-add_action( 'widgets_init', 'odin_widgets_init' );
+add_action('widgets_init', 'odin_widgets_init');
 
 /**
  * Flush Rewrite Rules for new CPTs and Taxonomies.
  *
  * @since 2.2.0
  */
-function odin_flush_rewrite() {
-	flush_rewrite_rules();
+function odin_flush_rewrite()
+{
+    flush_rewrite_rules();
 }
 
-add_action( 'after_switch_theme', 'odin_flush_rewrite' );
+add_action('after_switch_theme', 'odin_flush_rewrite');
 
 /**
  * Load site scripts.
  *
  * @since 2.2.0
  */
-function odin_enqueue_scripts() {
-	$template_url = get_template_directory_uri();
+function odin_enqueue_scripts()
+{
+    $template_url = get_template_directory_uri();
 
-	// Loads Odin main stylesheet.
-	wp_enqueue_style( 'odin-style', get_stylesheet_uri(), array(), null, 'all' );
+    // Loads Odin main stylesheet.
+    wp_enqueue_style('odin-style', get_stylesheet_uri(), array(), null, 'all');
 
-	// jQuery.
-	wp_enqueue_script( 'jquery' );
+    // jQuery.
+    wp_enqueue_script('jquery');
 
-	// Html5Shiv
-	wp_enqueue_script( 'html5shiv', $template_url . '/assets/js/html5.js' );
-	wp_script_add_data( 'html5shiv', 'conditional', 'lt IE 9' );
+    // Html5Shiv
+    wp_enqueue_script('html5shiv', $template_url . '/assets/js/html5.js');
+    wp_script_add_data('html5shiv', 'conditional', 'lt IE 9');
 
-	// General scripts.
-	if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) {
-		// Bootstrap.
-		// wp_enqueue_script( 'bootstrap', $template_url . '/assets/js/libs/bootstrap.min.js', array(), null, true );
+    // General scripts.
+    if (defined('SCRIPT_DEBUG') && SCRIPT_DEBUG) {
+        // Bootstrap.
+        // wp_enqueue_script( 'bootstrap', $template_url . '/assets/js/libs/bootstrap.min.js', array(), null, true );
 
-		// FitVids.
-		wp_enqueue_script( 'fitvids', $template_url . '/assets/js/libs/jquery.fitvids.js', array(), null, true );
+        // FitVids.
+        wp_enqueue_script('fitvids', $template_url . '/assets/js/libs/jquery.fitvids.js', array(), null, true);
 
-		// Main jQuery.
-		// wp_enqueue_script( 'odin-main', $template_url . '/assets/js/main.js', array(), null, true );
-	} else {
-		// Grunt main file with Bootstrap, FitVids and others libs.
-		wp_enqueue_script( 'odin-main-min', $template_url . '/assets/js/main.min.js', array(), null, true );
-	}
+        // Main jQuery.
+        // wp_enqueue_script( 'odin-main', $template_url . '/assets/js/main.js', array(), null, true );
+    } else {
+        // Grunt main file with Bootstrap, FitVids and others libs.
+        wp_enqueue_script('odin-main-min', $template_url . '/assets/js/main.min.js', array(), null, true);
+    }
 
-	// Grunt watch livereload in the browser.
-	// wp_enqueue_script( 'odin-livereload', 'http://localhost:35729/livereload.js?snipver=1', array(), null, true );
+    // Grunt watch livereload in the browser.
+    // wp_enqueue_script( 'odin-livereload', 'http://localhost:35729/livereload.js?snipver=1', array(), null, true );
 
-	// Load Thread comments WordPress script.
-	if ( is_singular() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+    // Load Thread comments WordPress script.
+    if (is_singular() && get_option('thread_comments')) {
+        wp_enqueue_script('comment-reply');
+    }
 }
 
-add_action( 'wp_enqueue_scripts', 'odin_enqueue_scripts', 1 );
+add_action('wp_enqueue_scripts', 'odin_enqueue_scripts', 1);
 
 /**
  * Odin custom stylesheet URI.
@@ -265,11 +269,12 @@ add_action( 'wp_enqueue_scripts', 'odin_enqueue_scripts', 1 );
  *
  * @return string      New URI.
  */
-function odin_stylesheet_uri( $uri, $dir ) {
-	return $dir . '/assets/css/style.css';
+function odin_stylesheet_uri($uri, $dir)
+{
+    return $dir . '/assets/css/style.css';
 }
 
-add_filter( 'stylesheet_uri', 'odin_stylesheet_uri', 10, 2 );
+add_filter('stylesheet_uri', 'odin_stylesheet_uri', 10, 2);
 
 /**
  * Query WooCommerce activation
@@ -278,10 +283,11 @@ add_filter( 'stylesheet_uri', 'odin_stylesheet_uri', 10, 2 );
  *
  * @return boolean
  */
-if ( ! function_exists( 'is_woocommerce_activated' ) ) {
-	function is_woocommerce_activated() {
-		return class_exists( 'woocommerce' ) ? true : false;
-	}
+if (!function_exists('is_woocommerce_activated')) {
+    function is_woocommerce_activated()
+    {
+        return class_exists('woocommerce') ? true : false;
+    }
 }
 
 /**
@@ -312,92 +318,172 @@ require_once get_template_directory() . '/inc/template-tags.php';
 /**
  * WooCommerce compatibility files.
  */
-if ( is_woocommerce_activated() ) {
-	add_theme_support( 'woocommerce' );
-	require get_template_directory() . '/inc/woocommerce/hooks.php';
-	require get_template_directory() . '/inc/woocommerce/functions.php';
-	require get_template_directory() . '/inc/woocommerce/template-tags.php';
+if (is_woocommerce_activated()) {
+    add_theme_support('woocommerce');
+    require get_template_directory() . '/inc/woocommerce/hooks.php';
+    require get_template_directory() . '/inc/woocommerce/functions.php';
+    require get_template_directory() . '/inc/woocommerce/template-tags.php';
 }
 
+function scripts()
+{
+    wp_enqueue_style('Montserrat', 'https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+    wp_enqueue_style('font-awesome.min', 'https://use.fontawesome.com/releases/v5.8.2/css/all.css');
+    wp_enqueue_style('bootstrapv4', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css');
 
-function scripts(){
-	wp_enqueue_style('Montserrat', 'https://fonts.googleapis.com/css?family=Montserrat&display=swap' );
-	wp_enqueue_style('font-awesome.min' , 'https://use.fontawesome.com/releases/v5.8.2/css/all.css');
-	wp_enqueue_style('bootstrapv4' , 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css');
-
-	wp_enqueue_script("jqueryv331" , "https://code.jquery.com/jquery-3.3.1.slim.min.js", array(), '', true );
-	wp_enqueue_script("popper" , "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js", array(), '', true );
-	wp_enqueue_script("bootstrapjs" , "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js", array(), '', true );
+    wp_enqueue_script("jqueryv331", "https://code.jquery.com/jquery-3.3.1.slim.min.js", array(), '', true);
+    wp_enqueue_script("popper", "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js", array(), '', true);
+    wp_enqueue_script("bootstrapjs", "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js", array(), '', true);
 }
-add_action( 'wp_enqueue_scripts', 'scripts' );
+add_action('wp_enqueue_scripts', 'scripts');
 
 /** POST TYPE Dentistas**/
-function dentista_cpt() {
-    $video = new Odin_Post_Type(
+function dentista_cpt()
+{
+    $cpt = new Odin_Post_Type(
         'Dentista', // Nome (Singular) do Post Type.
         'dentista' // Slug do Post Type.
     );
 
-    $video->set_labels(
+    $cpt->set_labels(
         array(
-            'menu_name' => __( 'Dentistas', 'odin' )
+            'menu_name' => __('Dentistas', 'odin'),
         )
     );
 
-    $video->set_arguments(
+    $cpt->set_arguments(
         array(
-            'supports' => array( 'title', 'editor', 'thumbnail' ),
-			'menu_icon' => 'dashicons-businessperson'
+            'supports' => array('title', 'editor', 'thumbnail'),
+            'menu_icon' => 'dashicons-businessperson',
         )
     );
 }
 
-add_action( 'init', 'dentista_cpt', 1 );
+add_action('init', 'dentista_cpt', 1);
 
 /** POST TYPE Serviços**/
-function servico_cpt() {
-    $video = new Odin_Post_Type(
+function servico_cpt()
+{
+    $cpt = new Odin_Post_Type(
         'Serviço', // Nome (Singular) do Post Type.
         'servico' // Slug do Post Type.
     );
 
-    $video->set_labels(
+    $cpt->set_labels(
         array(
-            'menu_name' => __( 'Serviços', 'odin' )
+            'menu_name' => __('Serviços', 'odin'),
         )
     );
 
-    $video->set_arguments(
+    $cpt->set_arguments(
         array(
-            'supports' => array( 'title', 'editor', 'thumbnail' ),
-			'menu_icon' => 'dashicons-lightbulb
-			'
+            'supports' => array('title', 'editor', 'thumbnail'),
+            'menu_icon' => 'dashicons-lightbulb
+			',
         )
     );
 }
 
-add_action( 'init', 'servico_cpt', 1 );
+add_action('init', 'servico_cpt', 1);
 
 /** POST TYPE Banner home**/
-function banner_home_cpt() {
-    $video = new Odin_Post_Type(
+function banner_home_cpt()
+{
+    $cpt = new Odin_Post_Type(
         'Banner', // Nome (Singular) do Post Type.
         'bannerhome' // Slug do Post Type.
     );
 
-    $video->set_labels(
+    $cpt->set_labels(
         array(
-            'menu_name' => __( 'Banners Home', 'odin' )
+            'menu_name' => __('Banners Home', 'odin'),
         )
     );
 
-    $video->set_arguments(
+    $cpt->set_arguments(
         array(
-			'supports' => array( 'title', 'editor', 'thumbnail' ),
-			'menu_icon' => 'dashicons-images-alt2'
+            'supports' => array('title', 'editor', 'thumbnail'),
+            'menu_icon' => 'dashicons-images-alt2',
         )
     );
 }
 
-add_action( 'init', 'banner_home_cpt', 1 );
+add_action('init', 'banner_home_cpt', 1);
 
+/* metabox serviços em destistas */
+function dentista_mt(){
+
+	$title_servicos = [];
+	$id_servicos = [];
+
+	$args = array(
+        'post_type' => 'servico',
+        'posts_per_page' => -1,
+    );
+    $posts = new WP_Query($args);
+    while ($posts->have_posts()): $posts->the_post();
+
+	array_push($title_servicos, get_the_title());
+    array_push($id_servicos, get_the_ID());
+
+    endwhile;
+	wp_reset_query();
+	
+	$servicos = array_combine($id_servicos, $title_servicos);
+
+    $metabox = new Odin_Metabox(
+        'servicos', // Slug/ID do Metabox (obrigatório)
+        'Serviços', // Nome do Metabox  (obrigatório)
+        'dentista', // Slug do Post Type, sendo possível enviar apenas um valor ou um array com vários (opcional)
+        'normal', // Contexto (opções: normal, advanced, ou side) (opcional)
+        'high' // Prioridade (opções: high, core, default ou low) (opcional)
+    );
+
+    $metabox->set_fields(
+        array(
+            array(
+                'id' => 'servicos1', // Obrigatório
+                'label' => __('Serviços 1', 'odin'), // Obrigatório
+                'type' => 'select', // Obrigatório
+                'default' => '', // Opcional
+                // 'description' => __('Descrition Example', 'odin'), // Opcional
+                'options' => $servicos
+            ),
+
+            array(
+                'id' => 'servicos2', // Obrigatório
+                'label' => __('Serviços 2', 'odin'), // Obrigatório
+                'type' => 'select', // Obrigatório
+                // 'attributes' => array(), // Opcional (atributos para input HTML/HTML5)
+                'default' => '', // Opcional
+                // 'description' => __('Descrition Example', 'odin'), // Opcional
+                'options' => $servicos
+            ),
+        )
+    );
+}
+add_action('init', 'dentista_mt', 1);
+
+/** POST TYPE depoimento**/
+function depoimento_cpt()
+{
+    $cpt = new Odin_Post_Type(
+        'Depoimento', // Nome (Singular) do Post Type.
+        'depoimento' // Slug do Post Type.
+    );
+
+    $cpt->set_labels(
+        array(
+            'menu_name' => __('Depoimentos', 'odin'),
+        )
+    );
+
+    $cpt->set_arguments(
+        array(
+            'supports' => array('title', 'editor', 'thumbnail'),
+            'menu_icon' => 'dashicons-images-alt2',
+        )
+    );
+}
+
+add_action('init', 'depoimento_cpt', 1);
--- themes/clinica/page-dentistas.php
+++ themes/clinica/page-dentistas.php
@@ -5,20 +5,26 @@
 </section>
 
 <?php
-    $args = array(
-        'post_type' => 'dentista',
-        'posts_per_page' => -1,
-    );
-    $posts = new WP_Query($args);
-    while ($posts->have_posts()): $posts->the_post();
+$args = array(
+    'post_type' => 'dentista',
+    'posts_per_page' => -1,
+);
+$posts = new WP_Query($args);
+while ($posts->have_posts()): $posts->the_post();
+    $servico1 = get_post(get_post_meta(get_the_ID(), 'servicos1', true));
+    $servico2 = get_post(get_post_meta(get_the_ID(), 'servicos2', true));
 
-        echo get_the_title() . "<br/>"; // titulo do dentista
-        echo get_the_content() . "<br/>"; // textinho do dentista
-        echo get_the_post_thumbnail() . "<br/>"; // foto do dentista
-        echo get_the_permalink() . "<br/>"; // link do dentista
+    echo $servico1->post_title. "<br/>"; //nome do serviço 1
+    echo get_permalink(get_post_meta(get_the_ID(), 'servicos1', true)) . "<br/>"; //link do serviço 1
+    echo $servico2->post_title. "<br/>"; //nome do serviço 2
+    echo get_permalink(get_post_meta(get_the_ID(), 'servicos2', true)) . "<br/>"; //link do serviço 2
+    echo get_the_title() . "<br/>"; // titulo do dentista
+    echo get_the_content() . "<br/>"; // textinho do dentista
+    echo get_the_post_thumbnail() . "<br/>"; // foto do dentista
+    echo get_the_permalink() . "<br/>"; // link do dentista
 
-    endwhile;
-    wp_reset_query();
+endwhile;
+wp_reset_query();
 ?>
 
-<?php echo footer(); ?>
+<?php get_footer();?>
